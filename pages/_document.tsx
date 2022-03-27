ea870e44165dfbd285889b06ba012dbfc231e124
aspnxdd <43625217+aspnxdd@users.noreply.github.com>
2022-03-27 17:29:31 +0200

    added head

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+      <title>Arnau Espin</title>
+
+        <link rel="icon" href="/p.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
